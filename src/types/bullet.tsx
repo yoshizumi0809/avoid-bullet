0e5acd171d2ff82b136334594796273314dbe285
yoshizumi <Yoshizumi@two-six.jp>
2025-12-16 18:02:55 +0900

    12/16 ホーミング弾実装完了

--- src/types/bullet.tsx
+++ src/types/bullet.tsx
@@ -1,7 +1,10 @@
-export type Bullet = {
-    id: number;
+export type BulletType = 'normal' | 'homing';
+
+export interface Bullet {
+    id: string; // または number
     x: number;
     y: number;
     vx: number;
     vy: number;
-};
+    type: BulletType; // 👈 これを追加！
+}
